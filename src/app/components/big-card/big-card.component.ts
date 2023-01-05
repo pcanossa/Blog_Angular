cf8359635b2d007579ccdad1473754e3157bb60d
Patrícia Canossa <p.canossa@hotmail.com>
2023-01-05 17:24:11 -0300

    Algumas mudanças de estilo e rrealização de responsividade dos cards

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
@@ -7,7 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BigCardComponent implements OnInit {
 
-  linkImg: string = '';
+  @Input()
+  linkImg: string = 'https://th.bing.com/th/id/R.dd4dabd8a109e6c449d931bc43da58f9?rik=Ow6%2fWiAVYb2WRg&riu=http%3a%2f%2fwww.viralnovelty.net%2fwp-content%2fuploads%2f2014%2f07%2f140.jpg&ehk=XHf7a30yumIvQc7NkICKuzdlu13Bg5pHDL9ckRHNKbM%3d&risl=&pid=ImgRaw&r=0';
+  @Input()
+  cardTitle: string = 'MAR E SUA SAÚDE';
+  @Input()
+  cardDescription: string = 'Ir a praia é muito bom, mas você sabia que um bom banho de mar também pode trazer benefícios a sua saúde?'
 
   constructor() { }
 
