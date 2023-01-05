1526533782c9b98fef13156a7cb3b6f7642e10f5
Patrícia Canossa <p.canossa@hotmail.com>
2023-01-05 18:23:22 -0300

    Algumas mudanças de estilo, criação da page content e criação de rota

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -8,11 +8,11 @@ import { Component, Input, OnInit } from '@angular/core';
 export class BigCardComponent implements OnInit {
 
   @Input()
-  linkImg: string = 'https://th.bing.com/th/id/R.dd4dabd8a109e6c449d931bc43da58f9?rik=Ow6%2fWiAVYb2WRg&riu=http%3a%2f%2fwww.viralnovelty.net%2fwp-content%2fuploads%2f2014%2f07%2f140.jpg&ehk=XHf7a30yumIvQc7NkICKuzdlu13Bg5pHDL9ckRHNKbM%3d&risl=&pid=ImgRaw&r=0';
+  linkImg: string = '';
   @Input()
-  cardTitle: string = 'MAR E SUA SAÚDE';
+  cardTitle: string = '';
   @Input()
-  cardDescription: string = 'Ir a praia é muito bom, mas você sabia que um bom banho de mar também pode trazer benefícios a sua saúde?'
+  cardDescription: string = ''
 
   constructor() { }
 
